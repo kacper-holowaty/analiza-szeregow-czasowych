b01901b231c69fea0369cd6736ece52e40753ba0
Kacper Hołowaty <kacper.holowaty@gmail.com>
2025-11-11 13:51:13 +0100

    :art: Fix R file for lab01

--- lab01/r/grecja_gva.R
+++ lab01/r/grecja_gva.R
@@ -49,53 +49,6 @@ mdane[ind.q3, 4] = mdane[ind.q3, 3] - ows.q3
 mdane[ind.q4, 4] = mdane[ind.q4, 3] - ows.q4
 mdane
 
-# wykres
-n = nrow(mdane)
-czas = 1:n
-
-# czyszczenie przestrzeni roboczej 
-rm(list=ls(all=TRUE))
-# zassanie danych
-dane=read.table("C:/Users/kacpe/OneDrive/Dokumenty/grecja-gva.csv", header=TRUE, sep=";", dec=",", fileEncoding= "Windows-1250")
-dane
-# utworzenie macierzy danych
-mdane=as.matrix(dane)
-mdane
-# pierwsze różnicowanie w celu usunięcia trendu
-d_gva = mdane[2:nrow(mdane), 2] - mdane[1:(nrow(mdane)-1), 2]
-length(d_gva)
-mdane = cbind(mdane, c(NA, d_gva))
-# wliczenie wskaźników sezonowości
-# surowe
-daty = as.character(mdane[,1])
-daty6 = substr(daty, start = 6, stop = 6)
-ind.q1 = (daty6 == "1")
-ind.q2 = (daty6 == "2")
-ind.q3 = (daty6 == "3")
-ind.q4 = (daty6 == "4")
-sws.q1 = mean(mdane[ind.q1,3], na.rm = T)
-sws.q2 = mean(mdane[ind.q2,3], na.rm = T)
-sws.q3 = mean(mdane[ind.q3,3], na.rm = T)
-sws.q4 = mean(mdane[ind.q4,3], na.rm = T)
-sws.q1
-sws.q2
-sws.q3
-sws.q4
-# oczyszczone
-mean.sws = mean(c(sws.q1, sws.q2, sws.q3, sws.q4))
-mean.sws
-ows.q1 = sws.q1 - mean.sws
-ows.q2 = sws.q2 - mean.sws
-ows.q3 = sws.q3 - mean.sws
-ows.q4 = sws.q4 - mean.sws
-# wyrównanie sezonowe
-mdane=cbind(mdane, NA)
-mdane[ind.q1, 4] = mdane[ind.q1, 3] - ows.q1
-mdane[ind.q2, 4] = mdane[ind.q2, 3] - ows.q2
-mdane[ind.q3, 4] = mdane[ind.q3, 3] - ows.q3
-mdane[ind.q4, 4] = mdane[ind.q4, 3] - ows.q4
-mdane
-
 # wykres główny
 n = nrow(mdane)
 czas = 1:n
@@ -123,4 +76,3 @@ par(xpd=FALSE)
 
 # przywrocenie marginesow
 par(mar=c(5,4,4,2))
-
